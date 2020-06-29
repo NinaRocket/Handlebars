146f85d42fe79f94a9c283a2ce05c7b7e0998361
NRocket <ninamrocket@gmail.com>
2020-06-29 07:32:24 -0700

    files are setup

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,15 @@
+const orm = require("../config/orm.js");
+//create the code that will all the ORM functions using burger specific input for the ORM
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = cat;
